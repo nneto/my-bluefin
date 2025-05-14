6f978d379ebe8a05b2bd1dd4658f0d8fd0cba5ec
Noberto Pessoa <7104467+nneto@users.noreply.github.com>
2025-05-13 22:25:30 -0300

    add starship

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-set -ouex pipefail
+set ${SET_X:+-x} -eou pipefail
 
 trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
 
 log() {
-  echo "=== $* ==="
+    echo -e "\n\n=== $* ===\n\n"
 }
 
 ### Install packages
+log "Starting packages installation"
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-log "Starting packages installation"
-
-# this installs a package from fedora repos
-dnf5 install -y tmux
-
-# MEGA
-mkdir -p /var/opt/megasync
-wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && dnf5 install -y //megasync-Fedora_42.x86_64.rpm
-wget https://mega.nz/linux/repo/Fedora_41/x86_64/nautilus-megasync-Fedora_41.x86_64.rpm && dnf5 install -y //nautilus-megasync-Fedora_41.x86_64.rpm
 
 # Use a COPR Example:
 #
@@ -32,9 +23,32 @@ wget https://mega.nz/linux/repo/Fedora_41/x86_64/nautilus-megasync-Fedora_41.x86
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+# COPR repos
+log "Enabling COPR"
+COPR_REPOS=(
+    atim/starship
+)
+dnf5 -y copr enable "${COPR_REPOS[@]}"
+
+# Layered Applications
+log "Installing RPM packages"
+LAYERED_PACKAGES=(
+    starship
+    tmux
+)
+dnf5 install --setopt=install_weak_deps=False -y "${LAYERED_PACKAGES[@]}"
+
+# MEGA
+mkdir -p /var/opt/megasync
+wget https://mega.nz/linux/repo/Fedora_42/x86_64/megasync-Fedora_42.x86_64.rpm && dnf5 install -y //megasync-Fedora_42.x86_64.rpm
+wget https://mega.nz/linux/repo/Fedora_41/x86_64/nautilus-megasync-Fedora_41.x86_64.rpm && dnf5 install -y //nautilus-megasync-Fedora_41.x86_64.rpm
+
 log "Enabling System Unit Files"
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
+
+log "Disabling COPR"
+dnf5 -y copr disable "${COPR_REPOS[@]}"
 
 log "Build completed"
--- build_files/fix-opt.sh
+++ build_files/fix-opt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -ouex pipefail
+set ${SET_X:+-x} -eou pipefail
 
 trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
 
 log() {
-  echo "=== $* ==="
+    echo -e "\n\n=== $* ===\n\n"
 }
 
 log "Starting /opt directory fix"
